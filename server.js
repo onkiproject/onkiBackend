0c83b6742fc877f8ae5f40d7624e90e918a4e1b6
Yewon Yang <yewoniese916@gmail.com>
2024-09-04 14:17:54 +0900

    0904-2

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.set('view engine', 'ejs')
 app.use("/public", express.static("public"));
 
 const dotenv = require('dotenv').config();
+const port = 8080;
 
 const mongoclient = require('mongodb').MongoClient;
 const ObjId = require('mongodb').ObjectId;
@@ -24,8 +25,8 @@ const url = "mongodb+srv://onki24:kCnkn4sTNQ5Hdu4u@onki-01.abw1d.mongodb.net/";
 mongoclient.connect(url)
     .then(client => {
         mydb = client.db('myboard');
-        app.listen(process.env.PORT, function(){
-            console.log("포트 8080으로 서버 대기중...");
+        app.listen(port, function(){
+            console.log(`${port}`);
         });
     }).catch(err => {
         console.log(err);
